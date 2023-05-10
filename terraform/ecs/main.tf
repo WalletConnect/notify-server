1280fdf23b1beff3a22379ecc16fe1f979640ee3
Rakowskiii <maciejrak@protonmail.com>
2023-05-10 13:26:07 +0200

    fix: better log level

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -63,7 +63,7 @@ resource "aws_ecs_task_definition" "app_task_definition" {
       ],
       environment = [
         { name = "PORT", value = "8080" },
-        { name = "LOG_LEVEL", value = "4" },
+        { name = "LOG_LEVEL", value = "info" },
         { name = "TELEMETRY_ENABLED", value = "true" },
         { name = "TELEMETRY_PROMETHEUS_PORT", value = local.prometheus_port },
         { name = "OTEL_EXPORTER_OTLP_ENDPOINT", value = "http://localhost:4317" },
