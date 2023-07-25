9c8dfa891ae686f6b05ce66e8aca9f1c37658a59
Rakowskiii <38994251+Rakowskiii@users.noreply.github.com>
2023-07-25 17:14:38 +0200

    fix: remove redundant jsonrpc code (#101)

--- src/jsonrpc.rs
+++ src/jsonrpc.rs
@@ -1,29 +1,7 @@
 use {
-    crate::types::{Notification, Subscribtion, Unsubscribe},
+    crate::types::Notification,
     serde::{Deserialize, Serialize},
 };
-
-pub type Topic = String;
-/// Data structure representing PublishParams.
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub struct PublishParams {
-    /// Topic to publish to.
-    pub topic: Topic,
-    /// Message to publish.
-    pub message: String,
-    /// Duration for which the message should be kept in the mailbox if it
-    /// be delivered, in seconds.
-    #[serde(rename = "ttl")]
-    pub ttl_secs: u32,
-    // #[serde(default, skip_serializing_if = "is_default")]
-    /// A label that identifies what type of message is sent based on the RPC
-    /// method used.
-    pub tag: u32,
-    /// A flag that identifies whether the server should trigger a
-    /// webhook to a client through a push server.
-    #[serde(default)]
-    pub prompt: bool,
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct JsonRpcPayload {
@@ -36,12 +14,6 @@ pub struct JsonRpcPayload {
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "method", content = "params")]
 pub enum JsonRpcParams {
-    #[serde(rename = "irn_publish", alias = "iridium_publish")]
-    Publish(PublishParams),
     #[serde(rename = "wc_pushMessage")]
     Push(Notification),
-    #[serde(rename = "irn_subscribe")]
-    Subscribe(Subscribtion),
-    #[serde(rename = "irn_unsubscribe")]
-    Unsubscribe(Unsubscribe),
 }
