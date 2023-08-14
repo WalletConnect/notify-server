95a6441f0d28361f74d479c1b6c25a71deba0caf
Chris Smith <chris@walletconnect.com>
2023-08-14 13:28:29 -0400

    fix: re-enable Grafana dashboard

--- terraform/monitoring/dashboard.tf
+++ terraform/monitoring/dashboard.tf
@@ -1,25 +1,24 @@
-# TODO: Restore this, was causing issues with the dashboard not being able to be created
-#data "jsonnet_file" "dashboard" {
-#  source = "${path.module}/dashboard.jsonnet"
+data "jsonnet_file" "dashboard" {
+ source = "${path.module}/dashboard.jsonnet"
 
-#  ext_str = {
-#    dashboard_title = "${var.environment} - Notify Server"
-#    dashboard_uid   = "${var.environment}-${var.app_name}"
+ ext_str = {
+   dashboard_title = "${var.environment} - Notify Server"
+   dashboard_uid   = "${var.environment}-${var.app_name}"
 
-#    prometheus_uid = grafana_data_source.prometheus.uid
-#    cloudwatch_uid = grafana_data_source.cloudwatch.uid
+   prometheus_uid = grafana_data_source.prometheus.uid
+   cloudwatch_uid = grafana_data_source.cloudwatch.uid
 
-#    environment      = var.environment
-#    notifications    = jsonencode(local.notifications)
-#    ecs_service_name = "${var.environment}-${var.app_name}-service"
-#    load_balancer    = local.load_balancer
-#    target_group     = local.target_group
-#    docdb_cluster_id = var.docdb_cluster_id
-#  }
-#}
+   environment      = var.environment
+   notifications    = jsonencode(local.notifications)
+   ecs_service_name = "${var.environment}-${var.app_name}-service"
+   load_balancer    = local.load_balancer
+   target_group     = local.target_group
+   docdb_cluster_id = var.docdb_cluster_id
+ }
+}
 
-#resource "grafana_dashboard" "dashboard" {
-#  overwrite   = true
-#  message     = "Updated by Terraform"
-#  config_json = data.jsonnet_file.dashboard.rendered
-#}
+resource "grafana_dashboard" "dashboard" {
+ overwrite   = true
+ message     = "Updated by Terraform"
+ config_json = data.jsonnet_file.dashboard.rendered
+}
