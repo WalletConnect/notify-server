18e03c12eec7d2454f15ba35dd04e7a0d3bd1af1
Chris Smith <chris@walletconnect.com>
2023-10-17 17:33:39 -0400

    feat: noop

--- src/lib.rs
+++ src/lib.rs
@@ -187,10 +187,10 @@ pub async fn bootstrap(mut shutdown: broadcast::Receiver<()>, config: Configurat
     let mut websocket_service = WebsocketService::new(state_arc.clone(), wsclient, rx).await?;
 
     select! {
-        _ = axum::Server::bind(&private_addr).serve(private_app.into_make_service()) => info!("Terminating metrics service"),
+        _ = axum::Server::bind(&private_addr).serve(private_app.into_make_service()) => info!("Private server terminating"),
         _ = axum::Server::bind(&addr).serve(app.into_make_service_with_connect_info::<SocketAddr>()) => info!("Server terminating"),
         _ = shutdown.recv() => info!("Shutdown signal received, killing servers"),
-        e = websocket_service.run() => info!("Unregister service terminating {:?}", e),
+        e = websocket_service.run() => info!("Websocket service terminating {:?}", e),
         e = watcher_expiration_job(state_arc.clone()) => info!("Watcher expiration job terminating {:?}", e),
     }
 
