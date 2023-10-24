53488364f1da2b9ac4ba5ae8fa28686069f62766
Max Kalashnikoff <geekmaks@gmail.com>
2023-10-24 15:56:16 +0200

    chore: splitting up api server and publishing service entry points

--- src/bin/publisher.rs
+++ src/bin/publisher.rs
@@ -1,21 +1,19 @@
 use {
     dotenv::dotenv,
     notify_server::{config::Configuration, Result},
-    tokio::sync::broadcast,
     tracing_subscriber::fmt::format::FmtSpan,
 };
 
+/// Service for processing queued messages for publish
 #[tokio::main]
 async fn main() -> Result<()> {
-    let (_signal, shutdown) = broadcast::channel(1);
-    dotenv().expect("Failed to load .env file");
-
+    dotenv().ok();
     let config = Configuration::new().expect("Failed to load config!");
     tracing_subscriber::fmt()
-        .with_env_filter(&config.log_level)
+        .with_env_filter(config.log_level)
         .with_span_events(FmtSpan::CLOSE)
         .with_ansi(std::env::var("ANSI_LOGS").is_ok())
         .init();
-
-    notify_server::bootstrap(shutdown, config).await
+    Ok(())
+    // TODO: Implement publisher service runner
 }
---
+++ src/bin/server.rs
@@ -0,0 +1,22 @@
+use {
+    dotenv::dotenv,
+    notify_server::{config::Configuration, Result},
+    tokio::sync::broadcast,
+    tracing_subscriber::fmt::format::FmtSpan,
+};
+
+/// Notify API server
+#[tokio::main]
+async fn main() -> Result<()> {
+    let (_signal, shutdown) = broadcast::channel(1);
+    dotenv().expect("Failed to load .env file");
+
+    let config = Configuration::new().expect("Failed to load config!");
+    tracing_subscriber::fmt()
+        .with_env_filter(&config.log_level)
+        .with_span_events(FmtSpan::CLOSE)
+        .with_ansi(std::env::var("ANSI_LOGS").is_ok())
+        .init();
+
+    notify_server::bootstrap(shutdown, config).await
+}
