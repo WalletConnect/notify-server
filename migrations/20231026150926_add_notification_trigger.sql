ff954d8ac3d5be06c10bb3957033c657cee76c4a
Max Kalashnikoff <geekmaks@gmail.com>
2023-10-26 23:45:59 +0200

    chore: adding new migration for notification triggers

---
+++ migrations/20231026150926_add_notification_trigger.sql
@@ -0,0 +1,19 @@
+-- Function that sends a pg_notify with the notification id to process
+CREATE FUNCTION "notification_for_delivery" ()
+    RETURNS TRIGGER AS $$
+    BEGIN
+        PERFORM pg_notify('notification_for_delivery', NEW.id::text);
+        RETURN NEW;
+    END;
+$$ LANGUAGE PLPGSQL;
+
+-- Trigger to notify when a new notification to delivery is created
+CREATE TRIGGER "subscriber_notification_insert" AFTER INSERT ON "subscriber_notification"
+    FOR EACH ROW
+    EXECUTE FUNCTION "notification_for_delivery" ();
+
+-- Trigger to notify when a notification delivery status is updated to the `queued` 
+CREATE TRIGGER "subscriber_notification_update" AFTER UPDATE ON "subscriber_notification"
+    FOR EACH ROW
+    WHEN (OLD.status <> NEW.status AND NEW.status = 'queued')
+    EXECUTE FUNCTION "notification_for_delivery" ();
