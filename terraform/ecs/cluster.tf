ea80871dc5336d5c522e56cad1094fa8fe420aed
Chris Smith <chris@walletconnect.com>
2023-10-27 11:14:31 -0400

    fix: revert at-most-one replica guarantee introduced in 135c0df and 4ccc1af

--- terraform/ecs/cluster.tf
+++ terraform/ecs/cluster.tf
@@ -170,8 +170,6 @@ resource "aws_ecs_service" "app_service" {
   task_definition                    = aws_ecs_task_definition.app_task.arn
   launch_type                        = "FARGATE"
   desired_count                      = var.autoscaling_desired_count
-  deployment_maximum_percent         = 100 # guarantee no more than desired_count tasks are running at a time
-  deployment_minimum_healthy_percent = 0   # Fix "Both maximumPercent and minimumHealthyPercent cannot be 100 as this will block deployments."
   propagate_tags                     = "TASK_DEFINITION"
 
   # Wait for the service deployment to succeed
