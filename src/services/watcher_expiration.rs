a1e24e35562bba1814d02588d97f7c900e0b0cf4
Chris Smith <chris@walletconnect.com>
2023-11-01 16:23:20 -0400

    fix: decrease frequency of watcher_expiration_job

--- src/services/watcher_expiration.rs
+++ src/services/watcher_expiration.rs
@@ -2,23 +2,24 @@ use {
     crate::model::helpers::delete_expired_subscription_watchers,
     sqlx::PgPool,
     std::time::Duration,
+    tokio::time,
     tracing::{info, warn},
 };
 
 pub async fn watcher_expiration_job(postgres: PgPool) {
-    let mut interval = tokio::time::interval(Duration::from_secs(5 * 60));
+    let mut interval = time::interval(Duration::from_secs(60 * 60));
 
     loop {
         interval.tick().await;
-        info!("running expiry job");
+        info!("Running watcher_expiration_job");
         if let Err(e) = job(&postgres).await {
-            warn!("Error expiring watchers: {e} {e:?}");
+            warn!("Error running watcher_expiration_job: {e:?}");
         }
     }
 }
 
 async fn job(postgres: &PgPool) -> sqlx::Result<()> {
     let count = delete_expired_subscription_watchers(postgres).await?;
-    info!("expired watchers: {count}");
+    info!("Expired {count} watchers");
     Ok(())
 }
