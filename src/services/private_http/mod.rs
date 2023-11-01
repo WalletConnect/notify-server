75c2b47d02de1cb7a7cd37c96d8ba5dab5e57be1
Chris Smith <chris@walletconnect.com>
2023-11-01 16:27:39 -0400

    chore: remove long return type

--- src/services/private_http/mod.rs
+++ src/services/private_http/mod.rs
@@ -7,15 +7,14 @@ use {
 
 mod handlers;
 
-pub fn start(
-    telemetry_prometheus_port: Option<u16>,
-) -> hyper::Server<hyper::server::conn::AddrIncoming, axum::routing::IntoMakeService<axum::Router>>
-{
+pub async fn start(telemetry_prometheus_port: Option<u16>) -> Result<(), hyper::Error> {
     let private_port = telemetry_prometheus_port.unwrap_or(3001);
     let private_addr = SocketAddr::from(([0, 0, 0, 0], private_port));
     info!("Starting private server on {}", private_addr);
 
     let private_app = Router::new().route("/metrics", get(handler));
 
-    axum::Server::bind(&private_addr).serve(private_app.into_make_service())
+    axum::Server::bind(&private_addr)
+        .serve(private_app.into_make_service())
+        .await
 }
